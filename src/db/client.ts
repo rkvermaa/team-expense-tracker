be361e6a0671a66d540128ab037d8ae12853eaf9
ravi.verma <ravi.verma@bounteous.com>
2026-09-08 18:07:14 +0000

    fix: remove .js extension from schema import in db client

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 import { drizzle, type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 
-import * as schema from "./schema.js";
+import * as schema from "./schema";
 
 export type Db = BetterSQLite3Database<typeof schema> & {
   $client: Database.Database;
